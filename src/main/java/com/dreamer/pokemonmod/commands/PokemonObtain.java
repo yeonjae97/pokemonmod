2e942db15d20bfe7b710ec4335614ccfb5e713fe
yeonjae97 <yeonjae978@gmail.com>
2023-10-20 19:33:16 +0900

    추가 커밋 (포켓몬 지급 명령어 포켓몬 한글이름 되게끔 재구현)

--- src/main/java/com/dreamer/pokemonmod/commands/PokemonObtain.java
+++ src/main/java/com/dreamer/pokemonmod/commands/PokemonObtain.java
@@ -4,28 +4,45 @@ import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.pixelmonmod.api.Specification;
 import com.pixelmonmod.api.pokemon.PokemonSpecification;
 import com.pixelmonmod.api.pokemon.PokemonSpecificationProxy;
 import com.pixelmonmod.api.pokemon.requirement.impl.SpeciesRequirement;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.api.command.PixelmonCommandUtils;
+import com.pixelmonmod.pixelmon.api.data.DataSync;
 import com.pixelmonmod.pixelmon.api.events.PokedexEvent;
+import com.pixelmonmod.pixelmon.api.pokedex.PokedexEntry;
 import com.pixelmonmod.pixelmon.api.pokedex.PokedexRegistrationStatus;
-import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
-import com.pixelmonmod.pixelmon.api.storage.PCStorage;
-import com.pixelmonmod.pixelmon.api.storage.PlayerPartyStorage;
-import com.pixelmonmod.pixelmon.api.storage.StorageProxy;
+import com.pixelmonmod.pixelmon.api.pokemon.*;
+import com.pixelmonmod.pixelmon.api.pokemon.export.PokemonConverterProxy;
+import com.pixelmonmod.pixelmon.api.pokemon.species.Pokedex;
+import com.pixelmonmod.pixelmon.api.pokemon.species.Species;
+import com.pixelmonmod.pixelmon.api.registries.PixelmonSpecies;
+import com.pixelmonmod.pixelmon.api.registries.PixelmonTileEntities;
+import com.pixelmonmod.pixelmon.api.storage.*;
+import com.pixelmonmod.pixelmon.api.util.ITranslatable;
+import com.pixelmonmod.pixelmon.api.util.PixelmonPlayerUtils;
 import com.pixelmonmod.pixelmon.battles.BattleRegistry;
+import com.pixelmonmod.pixelmon.client.gui.pc.PokemonScreen;
+import com.pixelmonmod.pixelmon.client.gui.pokedex.PokedexScreen;
+import com.pixelmonmod.pixelmon.client.gui.spawner.PokemonListScreen;
+import com.pixelmonmod.pixelmon.command.PixelCommand;
 import com.pixelmonmod.pixelmon.command.impl.PokeGiveCommand;
+import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
 import com.pixelmonmod.pixelmon.listener.PlayerCommandListener;
+import com.pixelmonmod.pixelmon.quests.editor.args.ArgumentType;
+import com.pixelmonmod.pixelmon.world.generation.gyms.PokemonDefinition;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.ITextProperties;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextComponentUtils;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.*;
+import sun.reflect.generics.tree.ReturnType;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -33,12 +50,13 @@ import net.minecraft.util.text.TextComponentUtils;
  * @apiNote 포켓몬 지급 명령어
  *
  */
-public class PokemonObtain{
+public class PokemonObtain {
+
     public PokemonObtain(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(Commands.literal("포켓몬설정")
                 .then(Commands.literal("지급")
                         .then(Commands.argument("닉네임", StringArgumentType.string())
-                                .then(Commands.argument("포켓몬이름", StringArgumentType.string())
+                                .then(Commands.argument( "포켓몬이름", StringArgumentType.greedyString())
                                         .executes((command) -> {
 
                                             String nickName = command.getArgument("닉네임", String.class);
@@ -61,7 +79,15 @@ public class PokemonObtain{
         PlayerPartyStorage pps = StorageProxy.getParty(player);    // 프로필
 //        PCStorage pcs = StorageProxy.getPCForPlayer(player);                // 스토리지
 
-        PokemonSpecification spec = PokemonSpecificationProxy.create(pokemonName);
+        List<Species> species = Arrays.stream(Pokedex.actualPokedex).collect(Collectors.toList());
+        for (Species list : species){
+            if(pokemonName.equals(list.getLocalizedName())){
+                pokemonName = list.getName();
+                break;
+            }
+        }
+
+        PokemonSpecification spec = PokemonSpecificationProxy.create(pokemonName);  // 영어 이름
         if (!spec.getValue(SpeciesRequirement.class).isPresent()) {
             throw new CommandException(new StringTextComponent("그런 이름을 가진 포켓몬이 존재하지 않습니다."));
         }
